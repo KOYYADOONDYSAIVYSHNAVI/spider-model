951ed9a4943640288a7298fbaa1bdc110f141bb9
KOYYADOONDYSAIVYSHNAVI <koyya@uchicago.edu>
2024-07-15 20:20:49 -0500

    Navigation bar for the home page

--- spider-frontend/src/App.js
+++ spider-frontend/src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink, Routes } from 'react-router-dom';
 import './App.css';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <div className="nav-container">
+            <img src="https://falkor8.com/wp-content/uploads/2024/05/Website-Logos-1.png" className="App-logo" alt="logo" />
+            <nav>
+              <ul>
+                <li>
+                  <NavLink
+                    to="/"
+                    className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/about"
+                    className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/contact"
+                    className={({ isActive }) => (isActive ? 'active-link' : 'inactive-link')}
+                  >
+                    Contact
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+        <div className="App-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
