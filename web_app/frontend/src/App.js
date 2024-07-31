39bdea4e049144f43ad24b41aa3a5841fa4a6797
KOYYADOONDYSAIVYSHNAVI <koyya@uchicago.edu>
2024-07-31 22:30:56 +0530

    Added the basic user-info page frontend and connected with flask application

--- web_app/frontend/src/App.js
+++ web_app/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 import HomePage from './components/home';
 import UserPage from './components/user_info'
+import ESGPage from './components/esg_entry'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/user-info" element={<UserPage/>}/>
+        <Route path="/esg-initiative" element={<ESGPage/>}/>
       </Routes>
     </Router>
   );
